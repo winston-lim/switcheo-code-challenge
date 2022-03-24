e9bd5a5347026696bc0bcafbde40cfadeb5d3b77
winston-lim <winston_lim1@hotmail.com>
2022-03-25 01:11:57 +0800

    initial commit

--- problem5/APIConsumer.sol
+++ problem5/APIConsumer.sol
@@ -15,7 +15,9 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 contract APIConsumer is ChainlinkClient {
     using Chainlink for Chainlink.Request;
 
-    uint256[] public volumes;
+    mapping(bytes32 => bool) public resultReceivedMapping;
+    mapping(bytes32 => uint256) public resultMapping;
+    mapping(address => mapping(address => bytes32)) balances;
 
     address private oracle;
     bytes32 private jobId;
@@ -39,7 +41,7 @@ contract APIConsumer is ChainlinkClient {
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function getBalance(address walletAddr, address tokenAddr)
+    function makeRequest(address walletAddr, address tokenAddr)
         public
         returns (bytes32 requestId)
     {
@@ -62,26 +64,58 @@ contract APIConsumer is ChainlinkClient {
                 )
             )
         );
-        request.add("path", "tokens[:1].balance");
-        return sendChainlinkRequestTo(oracle, request, fee);
-    }
-
-    function getBalances(address walletAddr, address[] calldata tokenAddrs)
-        public
-    {
-        for (uint256 i = 0; i < tokenAddrs.length; i++) {
-            getBalance(walletAddr, tokenAddrs[i]);
-        }
+        request.add("path", "tokens[0].balance");
+        requestId = sendChainlinkRequestTo(oracle, request, fee);
     }
 
     /**
      * Receive the response in the form of uint256
      */
-    function fulfill(bytes32 _requestId, uint256 _volume)
+    function fulfill(bytes32 _requestId, uint256 _result)
         public
         recordChainlinkFulfillment(_requestId)
     {
-        volumes.push(_volume);
+        resultReceivedMapping[_requestId] = true;
+        resultMapping[_requestId] = _result;
+    }
+
+    function getResult(bytes32 _requestId)
+        public
+        view
+        returns (uint256 result)
+    {
+        result = resultMapping[_requestId];
+    }
+
+    function getResultReceived(bytes32 _requestId)
+        public
+        view
+        returns (bool resultReceived)
+    {
+        resultReceived = resultReceivedMapping[_requestId];
+    }
+
+    function fetchBalances(address walletAddr, address[] calldata tokenAddrs)
+        public
+    {
+        for (uint256 i = 0; i < tokenAddrs.length; i++) {
+            requestId = makeRequest(walletAddr, tokenAddrs[i]);
+            balances[tokenAddrs[i]] = requestId;
+        }
+    }
+
+    function getBalances(address walletAddr, address[] calldata tokenAddrs)
+        public
+        view
+        returns (uint256[] _balances)
+    {
+        uint256[] results;
+        for (uint256 i = 0; i < tokenAddrs.length; i++) {
+            if (getResultReceived(balances[walletAddr][tokenAddrs[i]])) {
+                results.push(getResult(balances[walletAddr][tokenAddrs[i]]));
+            }
+        }
+        return results;
     }
 
     // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
--- problem5/readme.md
+++ problem5/readme.md
@@ -14,9 +14,14 @@ NOTE: Spent >12 hours and did not complete task, so I attached my last attempt i
 - Eventually thought of using an API and found `api.ethplorer.io` which suits problem requirement
 - Realized that using APIs in contracts are not easy but went down that rabbit hole
 - Read documentations for Chainlink
-- (attempted) Wrote contact that makes multiple API calls to find balances for a wallet for each token address
-- (attempted) Deployed contract on Remix on Kovan Test Network
-- (attempted) Creating a transaction - failed and cannot debug
+- Attempted writing contact that makes multiple API calls to find balances for a wallet for each token address
+- Deployed contract using Remix on Kovan Test Network
+- Attempted creating a transaction - fails without much information on why
+- Found out that the error was due to the created contract not having any LINK tokens
+- Eventually was able to create transaction, but balances were not updated
+- Debugged for another few hours - found out that API calls were not made/fulfilled, due to Oracle Node somehow accepting job
+- Attempted to find Nodes that are active, but most still do not work
+- Having no means to test/debug, I decided to submit it in its current state
 
 ## Diffulties/ Lessons Learned
 
